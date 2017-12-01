0fdec4a42a1c57de93ced95a4c3235d7d455e862
munstein <eduardo.munstein@live.com>
2017-11-30 23:01:53 -0200

    Refactoring

--- app/src/main/java/com/munstein/demomvp/FakeBase.java
+++ app/src/main/java/com/munstein/demomvp/FakeBase.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by @Munstein on 18/10/2017. --03:04
+ * Created by @Munstein on 18/10/2017. --03:04  
  */
 
 public class FakeBase implements IRepository{
